034b1faa83009c9fac388289de64049b52e3ffe8
Shea-Scarlet <gea.loro@gmail.com>
2024-05-22 15:13:59 -0700

    header visualization

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
