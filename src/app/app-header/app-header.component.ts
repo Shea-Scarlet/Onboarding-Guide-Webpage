8ddd30a8a7f956b5e4450df722aac0154f731b22
Shea-Scarlet <gea.loro@gmail.com>
2024-05-22 13:48:02 -0700

    minor fix

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-app-header',
   standalone: true,
   imports: [
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss'
