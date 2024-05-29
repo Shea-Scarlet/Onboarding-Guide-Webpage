614c265594ce5fe8cc1428cf567a1a3d5c24ea21
Shea-Scarlet <gea.loro@gmail.com>
2024-05-29 14:08:17 -0700

    Deleted Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppOverviewComponent } from './app-overview/app-overview.component';
-import { PageComponent } from './page/page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  imports: [AppHeaderComponent, AppOverviewComponent, PageComponent],
+  imports: [AppHeaderComponent, AppOverviewComponent],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
