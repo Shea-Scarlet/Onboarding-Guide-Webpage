5c43e5a6f305577f9190cfe75a3146fdfecad33e
Shea-Scarlet <gea.loro@gmail.com>
2024-05-29 14:10:18 -0700

    created app-features

---
+++ src/app/app-features/app-features.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-features.component.html',
+  styleUrl: './app-features.component.scss'
+})
+export class AppFeaturesComponent {
+
+}
