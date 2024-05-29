c8a44753ebcd0e63c05f5ab363a0a6219045776a
Shea-Scarlet <gea.loro@gmail.com>
2024-05-29 14:09:03 -0700

    deleted page part 2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { PageComponent } from './app/page/page.component';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
 
