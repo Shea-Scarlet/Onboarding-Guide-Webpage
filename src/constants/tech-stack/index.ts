24c65b938b15518e469964d2e8887a512b150fab
yuki-1184 <yuki.1184apple@gmail.com>
2024-05-30 22:55:33 -0700

    Modified texts

--- src/constants/tech-stack/index.ts
+++ src/constants/tech-stack/index.ts
@@ -1,8 +1,8 @@
 export const FRONT_END_TITLE = "Web Frontend";
-export const FRONT_END_SUBTEXT = "core technologies used to build and structure the visual and interactive aspects of a web application."
+export const FRONT_END_SUBTEXT = "Core technologies used to build and structure the visual and interactive aspects of a web application."
 export const TEST_VISUALIZATION_TITLE = "Testing & Visualization";
-export const TEST_VISUALIZATION_SUBTEXT = "tools and practices for verifying that the web application works as intended.";
+export const TEST_VISUALIZATION_SUBTEXT = "Tools and practices for verifying that the web application works as intended.";
 export const LINTER_FORMATTER_TITLE = "Linter & Formatter";
-export const LINTER_FORMATTER_SUBTEXT = "tools that automatically analyze and format code to ensure consistency, readability, and adherence to best practices.";
+export const LINTER_FORMATTER_SUBTEXT = "Tools that automatically analyze and format code to ensure consistency, readability, and adherence to best practices.";
 export const DESIGN_TITLE = "Design";
-export const DESIGN_SUBTEXT = "tools used for creating and collaborating on the visual and user experience aspects of the application.";
+export const DESIGN_SUBTEXT = "Tools used for creating and collaborating on the visual and user experience aspects of the application.";
